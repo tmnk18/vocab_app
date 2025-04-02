408f968850248698ae2cb7fac3ff41ff8c4856b1
tmnk18 <t250181@gmail.com>
2025-04-02 20:52:21 +0900

    ユーザー登録でusernameを保存・表示できるように対応

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_01_123839) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_02_112740) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_01_123839) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
