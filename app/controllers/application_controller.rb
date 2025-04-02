0c31efb891f98fb7e810eb73c53bade937056a0c
tmnk18 <t250181@gmail.com>
2025-04-02 21:32:51 +0900

    ActiveStorageを導入し、ユーザーがプロフィール画像を登録・表示できるように対応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me, :avatar]
     devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
     devise_parameter_sanitizer.permit(:account_update, keys: added_attrs)
   end
