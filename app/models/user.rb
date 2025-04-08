b15fe837fa58fc717780c9e9bc08fd320de925e7
tmnk18 <t250181@gmail.com>
2025-04-08 22:17:01 +0900

    ユーザーのバリデーションを整備

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,11 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
 
+  # バリデーション
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :name, presence: true, length: { maximum: 50 }
+
   validate :avatar_format
 
   private
