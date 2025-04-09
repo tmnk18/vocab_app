ed4d6fe5c8c0c0db8e105561a7d1e11e65f1683a
tmnk18 <t250181@gmail.com>
2025-04-09 22:49:22 +0900

    ログイン後のリダイレクト先をフォルダ一覧ページに設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # ログイン後のリダイレクト先を指定
+  def after_sign_in_path_for(resource)
+    folders_path # フォルダ一覧ページにリダイレクト
+  end
+
   def configure_permitted_parameters
     added_attrs = [:username, :email, :password, :password_confirmation, :remember_me, :avatar]
     devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
