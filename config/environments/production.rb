4b87ea3bf1a1f5c6dd68030ffa2d07770c1151a8
tmnk18 <t250181@gmail.com>
2025-04-11 17:37:07 +0900

    Heroku用のProcfileと本番環境設定を追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,8 +26,8 @@ Rails.application.configure do
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
-  # Do not fall back to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # アセットのコンパイルを有効化
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
@@ -94,4 +94,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # ホスト名の許可
+  config.hosts.clear
 end
